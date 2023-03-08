b537fe9c48f1981cc9ab8cf75ca72578535a3859
Ilya QA-58 <ilyamaidanov58@yandex.ru>
2023-03-08 16:57:07 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int account = 100; // Сумма начального счета.
+        int paying = 1050;  // Сумма пополнения.
+        int bonus = paying / 100; // Кол-во начисленных бонусов.
+
+        if(paying >= 1000) {
+            System.out.println("Добрый день!");
+            System.out.println("Благодарим Вас за пополнение счета");
+            System.out.println("Вам начисленны бонусы в размере:" + " " + (bonus) + "руб.");
+            System.out.println("Итоговая сумма на Вашем счету составляет:" + " " + (account + paying + bonus) + "руб." );
+        } else{
+            System.out.println("Добрый день!");
+            System.out.println("Благодарим Вас за пополнение счета");
+            System.out.println("Итоговая сумма на Вашем счету составляет:" + " " + (account + paying) + "руб." );
+        }
+    }
+}
